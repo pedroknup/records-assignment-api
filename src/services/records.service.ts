89bb7249d7e81bf92c0fa1a56e3c308a4230b5f3
Pedro Knup <pedro.knup@mediamonks.com>
2023-08-30 10:27:01 +0200

    feat: improve duplicate record logic

--- src/services/records.service.ts
+++ src/services/records.service.ts
@@ -3,7 +3,11 @@ import { xmlToJson } from '../utils/xml-to-json.util';
 import { xmlToRecord } from '../utils/xml-json-to-record.util';
 import { csvToRecord } from '../utils/csv-to-record.util';
 
-export const processRecordsFile = async (recordsFile: Express.Multer.File, previouslyProcessedRecords?: ModelType[] ) => {
+const DUPLICATE_RECORD_NOTE = 'Duplicate record';
+const WRONG_BALANCE_NOTE = 'End balance does not match calculated end balance';
+
+
+export const processRecordsFile = async (recordsFile: Express.Multer.File) => {
   const fileExtension = recordsFile.originalname.split('.').pop();
 
   let records: ModelType[] = [];
@@ -19,7 +23,7 @@ export const processRecordsFile = async (recordsFile: Express.Multer.File, previ
       throw new Error('File extension not supported');
   }
 
-  return processRecords(records, previouslyProcessedRecords);
+  return processRecords(records);
 };
 
 export const processRecordsFiles = async (
@@ -36,37 +40,40 @@ export const processRecordsFiles = async (
   for (const fileOrArray of uploadedFiles) {
     const file = Array.isArray(fileOrArray) ? fileOrArray[0] : fileOrArray;
 
-    const _processedRecords = await processRecordsFile(file, processedRecords);
+    const _processedRecords = await processRecordsFile(file);
     processedRecords.push(..._processedRecords);
   }
 
   return processedRecords;
 };
  
-const processRecords = (records: ModelType[], previouslyProcessedRecords?: ModelType[]) => {
+const processRecords = (records: ModelType[]) => {
   const processedRecords: ModelType[] = [];
 
   for (const record of records) {
-    const { startBalance, mutation, endBalance } = record;
+    const { startBalance, mutation, endBalance, reference } = record;
     const calculatedEndBalance = parseFloat(
       (startBalance + mutation).toFixed(2)
+      );
+      
+    const duplicateRecordIndex = processedRecords?.findIndex(
+      (_record) => _record.reference === reference
     );
-
-    const existingRecord = [...(previouslyProcessedRecords || []), ...processedRecords].find((_record) => {
-      return _record.reference === record.reference;
-    });
+    const recordReferenceExists = duplicateRecordIndex !== -1;
 
     let validationNotes: string | undefined;
     let isValid = true;
 
-    if (existingRecord) {
+    if (recordReferenceExists) {
       isValid = false;
-      validationNotes = 'Duplicate record';
+      validationNotes = DUPLICATE_RECORD_NOTE;
+      processedRecords[duplicateRecordIndex].validationNotes = DUPLICATE_RECORD_NOTE;
+      processedRecords[duplicateRecordIndex].isValid = false;
     } else if (calculatedEndBalance !== endBalance) {
       isValid = false;
-      validationNotes = 'End balance does not match calculated end balance';
+      validationNotes = WRONG_BALANCE_NOTE;
     }
-
+    
     processedRecords.push({
       ...record,
       isValid,
